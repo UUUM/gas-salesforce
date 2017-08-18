2180f18bb5b02b96f143d790eec6a979ef828285
Masato Bito <masato@bz2.jp>
2017-08-18 19:20:43 +0900

    fix by eslint

--- src/Web.js
+++ src/Web.js
@@ -1,4 +1,4 @@
-function doGet(e) {
+function doGet() {
   var sf = (new TestCommon()).createSalesforce();
   return HtmlService.createHtmlOutput('<a href="' + sf.getAuthorizationUrl() + '" target="_blank">Authorize</a>');
 }
@@ -8,7 +8,8 @@ function authCallback(request) {
   var isAuthorized = sf.service.handleCallback(request);
   if (isAuthorized) {
     return HtmlService.createHtmlOutput('Success! You can close this tab.');
-  } else {
-    return HtmlService.createHtmlOutput('Denied. You can close this tab');
   }
+  return HtmlService.createHtmlOutput('Denied. You can close this tab');
 }
+
+/* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^(doGet|authCallback)$" }] */

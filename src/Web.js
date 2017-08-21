1b2f7cad3d7bc2041f51de22e25b504af349819a
Masato Bito <masato@bz2.jp>
2017-08-21 09:39:05 +0900

    fix

--- src/Web.js
+++ src/Web.js
@@ -1,9 +1,9 @@
 function doGet(e) {
-  return (new TestCommon()).createSalesforce().oauth2.doGet(e);
+  return (new TestCommon()).createClient().oauth2.doGet(e);
 }
 
 function authCallback(request) {
-  return (new TestCommon()).createSalesforce().oauth2.callback(request);
+  return (new TestCommon()).createClient().oauth2.callback(request);
 }
 
 /* eslint no-unused-vars: ["error", { "varsIgnorePattern": "^(doGet|authCallback)$" }] */

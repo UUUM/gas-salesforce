ab8082e701cac2060cf14fa1b8a7a3d173ef5de7
Masato Bito <masato@bz2.jp>
2017-08-22 20:38:40 +0900

    add tests

--- src/ClientTest.js
+++ src/ClientTest.js
@@ -64,6 +64,63 @@ testRunner.functions.push(function (test) {
     assert.equal(client.clientSecret, clientSecret, 'has a clientSecret property');
     assert.equal(client.option.contentType, 'application/json', 'has a valid content type');
     assert.equal(client.option.muteHttpExceptions, true, 'has a valid muteHttpExceptions value');
+    assert.ok(client.oauth2 instanceof OAuth2Client, 'has a oauth2 property');
+  });
+
+  test('Client.createQueryString()', function (assert) {
+    setup();
+
+    var queryString = client.createQueryString({});
+    assert.equal(queryString, '', 'returns an empty string with no parameters');
+
+    queryString = client.createQueryString({foo: 'bar', bar: 'baz'});
+    assert.equal(queryString, 'foo=bar&bar=baz', 'returns a query string');
+
+    queryString = client.createQueryString({foo: 'bar', ids: [1, 2]});
+    assert.equal(queryString, 'foo=bar&ids%5B0%5D=1&ids%5B1%5D=2', 'returns an array query string');
+  });
+
+  test('Client.getApiPath()', function (assert) {
+    setup();
+
+    assert.equal('/services/data/v40.0/query', client.getApiPath('query'), 'returns a valid api path');
+    assert.equal('/query', client.getApiPath('/query'), 'returns a path itself if it starts with /');
+  });
+
+  test('Client.getApiUrl()', function (assert) {
+    setup();
+
+    assert.equal(
+      client.getApiUrl('query'),
+      'https://uuum.my.salesforce.com/services/data/v40.0/query',
+      'returns a valid api url'
+    );
+
+    assert.equal(
+      client.getApiUrl('query', {q: 'SELECT'}),
+      'https://uuum.my.salesforce.com/services/data/v40.0/query?q=SELECT',
+      'returns a valid api url with query strings'
+    );
+  });
+
+  test('Client.getAuthorizationHeader()', function (assert) {
+    setup();
+
+    assert.deepEqual(
+      client.getAuthorizationHeader(),
+      { Authorization: 'Bearer ' + client.oauth2.getAccessToken() },
+      'returns a valid authorization header'
+    );
+  });
+
+  test('Client.objMerge()', function (assert) {
+    setup();
+
+    var a = {foo: 'foo'};
+    var b = {bar: 'bar'};
+    var c = {bar: 'baz'};
+    var expected = {foo: 'foo', bar: 'baz'};
+    assert.deepEqual(client.objMerge(a, b, c), expected, 'merges objects');
   });
 });
 

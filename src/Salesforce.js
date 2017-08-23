513208b14838e3087da9e9afd552c6eed984bcb1
Masato Bito <masato@bz2.jp>
2017-08-23 15:28:15 +0900

    add doGet(), callback() methods

--- src/Salesforce.js
+++ src/Salesforce.js
@@ -2,6 +2,14 @@ var Salesforce = function Salesforce(version, clientId, clientSecret) {
   this.client = new Client(version, clientId, clientSecret);
 };
 
+Salesforce.prototype.callback = function callback(request) {
+  return this.client.oauth2.callback(request);
+};
+
+Salesforce.prototype.doGet = function doGet(e) {
+  return this.client.oauth2.doGet(e);
+};
+
 Salesforce.prototype.query = function query(soql) {
   var response = this.client.fetchGet('query', { q: soql });
   if (response instanceof ResponseError) {

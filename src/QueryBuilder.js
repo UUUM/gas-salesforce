f65a43b4571feac592953a449897c19030bbe49c
Masato Bito <masato@bz2.jp>
2017-08-24 18:38:46 +0900

    add QueryBuilder class

---
+++ src/QueryBuilder.js
@@ -0,0 +1,115 @@
+var QueryBuilder = function QueryBuilder() {
+  this.fieldList = [];
+  this.groups = [];
+  this.orders = [];
+  this.parameters = {};
+};
+
+QueryBuilder.prototype.assignParams = function assignParams(query, params) {
+  return _.reduce(params, function replace(result, value, key) {
+    return result.replace(':' + key, value);
+  }, query);
+};
+
+QueryBuilder.prototype.getQuery = function getQuery() {
+  var query = 'SELECT ' + this.fieldList.join(', ') + ' FROM ' + this.sobject;
+
+  if (this.whereClause) {
+    query += ' WHERE ' + this.whereClause;
+  }
+
+  if (this.groups.length) {
+    query += ' GROUP BY ' + this.groups.join(', ');
+  }
+
+  if (this.orders.length) {
+    query += ' ORDER BY ' + this.orders.join(', ');
+  }
+
+  if (this.limit) {
+    query += ' LIMIT ' + this.limit;
+  }
+
+  if (this.offset) {
+    query += ' OFFSET ' + this.offset;
+  }
+
+  return this.assignParams(query, this.parameters);
+};
+
+QueryBuilder.prototype.fields = function fields(fieldList) {
+  if (!Obj.isArray(fieldList) && !Obj.isString(fieldList)) {
+    throw new Error('Fields must be an array or a string');
+  }
+
+  this.fieldList = this.fieldList.concat(fieldList);
+  return this;
+};
+
+QueryBuilder.prototype.from = function from(sobject) {
+  if (!Obj.isString(sobject)) {
+    throw new Error('SObject must be a string');
+  }
+
+  this.sobject = sobject;
+  return this;
+};
+
+QueryBuilder.prototype.groupBy = function groupBy(columns) {
+  if (!Obj.isArray(columns) && !Obj.isString(columns)) {
+    throw new Error('Group columns must be an array or a string');
+  }
+
+  this.groups = this.groups.concat(columns);
+  return this;
+};
+
+QueryBuilder.prototype.limit = function limit(x) {
+  if (!Obj.isInteger(x)) {
+    throw new Error('Limit must be an integer');
+  }
+
+  this.maxResults = x;
+  return this;
+};
+
+QueryBuilder.prototype.offset = function offset(x) {
+  if (!Obj.isInteger(x)) {
+    throw new Error('Offset must be an integer');
+  }
+
+  this.firstResult = x;
+  return this;
+};
+
+QueryBuilder.prototype.orderBy = function orderBy(column, order) {
+  if (!Obj.isString(column)) {
+    throw new Error('Column must be a string');
+  }
+
+  var orderUpperCase = order.toUpperCase();
+  if (orderUpperCase !== 'ASC' && orderUpperCase !== 'DESC') {
+    throw new Error('Order must be "ASC" or "DESC"');
+  }
+
+  this.orders = this.orders.concat(column + ' ' + orderUpperCase);
+  return this;
+};
+
+QueryBuilder.prototype.params = function params(parameters) {
+  if (!Obj.isObject(parameters)) {
+    throw new Error('Parameters must be an object');
+  }
+
+  this.parameters = Obj.merge(this.parameters, parameters);
+  return this;
+};
+
+QueryBuilder.prototype.where = function where(whereClause) {
+  if (!Obj.isString(whereClause)) {
+    throw new Error('Where clause must be a string');
+  }
+
+  this.whereClause = whereClause;
+  return this;
+};
---
+++ src/QueryBuilderTest.js
@@ -0,0 +1,216 @@
+testRunner.functions.push(function testFunc(test) {
+  test('new QueryBuilder()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.ok(qb instanceof QueryBuilder, 'creates QueryBuilder object');
+  });
+
+  test('QueryBuilder.fields()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.deepEqual(qb.fields, [], 'has an empty array');
+
+    assert.throws(
+      function f2() {
+        qb.fields(1);
+      },
+      'throws an exception if value was a number'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.fields({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.fields('foo'), qb, 'returns itself');
+    assert.deepEqual(qb.fieldList, ['foo'], 'recieves a string');
+
+    assert.equal(qb.fields(['bar', 'baz']), qb, 'returns itself');
+    assert.deepEqual(qb.fieldList, ['foo', 'bar', 'baz'], 'recieves an array');
+  });
+
+  test('QueryBuilder.from()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.notOk(qb.sobject, 'not have a sobject property');
+
+    assert.throws(
+      function f2() {
+        qb.from(1);
+      },
+      'throws an exception if value was a number'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.from([]);
+      },
+      'throws an exception if value was an array'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.from({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.from('Opportunity'), qb, 'returns itself');
+    assert.equal(qb.sobject, 'Opportunity', 'has a sobject property');
+
+    assert.equal(qb.from('Account'), qb, 'returns itself');
+    assert.equal(qb.sobject, 'Account', 'has a sobject property');
+  });
+
+  test('QueryBuilder.groupBy()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.deepEqual(qb.groups, [], 'has an empty array');
+
+    assert.throws(
+      function f2() {
+        qb.groupBy(1);
+      },
+      'throws an exception if value was a number'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.groupBy({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.groupBy('foo'), qb, 'returns itself');
+    assert.deepEqual(qb.groups, ['foo'], 'recieves a string');
+
+    assert.equal(qb.groupBy(['bar', 'baz']), qb, 'returns itself');
+    assert.deepEqual(qb.groups, ['foo', 'bar', 'baz'], 'has two groups');
+  });
+
+  test('QueryBuilder.limit()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.notOk(qb.maxResults, 'not have a maxResults property');
+
+    assert.throws(
+      function f2() {
+        qb.limit('');
+      },
+      'throws an exception if value was a string'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.limit([]);
+      },
+      'throws an exception if value was an array'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.limit({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.limit(1), qb, 'returns itself');
+    assert.equal(qb.maxResults, 1, 'has a maxResults property');
+
+    assert.equal(qb.limit(2), qb, 'returns itself');
+    assert.equal(qb.maxResults, 2, 'has a maxResults property');
+  });
+
+  test('QueryBuilder.offset()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.notOk(qb.firstResult, 'not have a firstResult property');
+
+    assert.throws(
+      function f2() {
+        qb.offset('');
+      },
+      'throws an exception if value was a string'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.offset([]);
+      },
+      'throws an exception if value was an array'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.offset({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.offset(1), qb, 'returns itself');
+    assert.equal(qb.firstResult, 1, 'has a firstResult property');
+
+    assert.equal(qb.offset(2), qb, 'returns itself');
+    assert.equal(qb.firstResult, 2, 'has a firstResult property');
+  });
+
+  test('QueryBuilder.params()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.deepEqual(qb.parameters, {}, 'has an empty object');
+
+    assert.throws(
+      function f2() {
+        qb.params(1);
+      },
+      'throws an exception if value was a number'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.params('');
+      },
+      'throws an exception if value was a string'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.params([]);
+      },
+      'throws an exception if value was an array'
+    );
+
+    assert.equal(qb.params({foo: 'bar'}), qb, 'returns itself');
+    assert.deepEqual(qb.parameters, {foo: 'bar'}, 'has a parameters property');
+
+    assert.equal(qb.params({bar: 'baz'}), qb, 'returns itself');
+    assert.deepEqual(qb.parameters, {foo: 'bar', bar: 'baz'}, 'has a parameters property');
+  });
+
+  test('QueryBuilder.where()', function f(assert) {
+    var qb = new QueryBuilder();
+    assert.notOk(qb.whereClause, 'not have a whereClause property');
+
+    assert.throws(
+      function f2() {
+        qb.where(1);
+      },
+      'throws an exception if value was a number'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.where([]);
+      },
+      'throws an exception if value was an array'
+    );
+
+    assert.throws(
+      function f2() {
+        qb.where({});
+      },
+      'throws an exception if value was an object'
+    );
+
+    assert.equal(qb.where('foo'), qb, 'returns itself');
+    assert.equal(qb.whereClause, 'foo', 'has a whereClause property');
+
+    assert.equal(qb.where('bar'), qb, 'returns itself');
+    assert.equal(qb.whereClause, 'bar', 'has a whereClause property');
+  });
+});

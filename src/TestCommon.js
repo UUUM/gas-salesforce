ba5453eeaee8b557ec9eb52137920072b5de6050
Masato Bito <masato@bz2.jp>
2017-09-15 16:41:48 +0900

    add Spreadsheet class

--- src/TestCommon.js
+++ src/TestCommon.js
@@ -5,6 +5,7 @@ var TestCommon = function TestCommon() {
 
   this.clientId = properties.getProperty('clientId');
   this.clientSecret = properties.getProperty('clientSecret');
+  this.spreadsheetId = properties.getProperty('spreadsheetId');
 };
 
 TestCommon.prototype.createClient = function createClient() {
@@ -13,4 +14,8 @@ TestCommon.prototype.createClient = function createClient() {
 
 TestCommon.prototype.createSalesforce = function createSalesforce() {
   return new Salesforce(this.version, this.clientId, this.clientSecret);
+};
+
+TestCommon.prototype.getSpreadsheet = function getSpreadsheet() {
+  return SpreadsheetApp.openById(this.spreadsheetId);
 };
